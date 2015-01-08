3f3271c6874f9d78f83f8cc14134afcadd1acd04
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-01-07 18:06:22 -0600

    move config to config file

--- app.py
+++ app.py
@@ -7,16 +7,7 @@ from FriendsAPI import FriendsListAPI
 from PasswordAPI import ChangePasswordAPI
 
 app = Flask(__name__)
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'flask-test',
-    'host': 'ds027741.mongolab.com',
-    'port': 27741,
-    'username': 'flask-admin',
-    'password': '123123'
-}
-
-app.config['REDIS_URL'] = "redis://:123123@pub-redis-17784.us-east-1-2.1.ec2.garantiadata.com:17784/0"
-app.config['SECRET_KEY'] = 'flask is cool' 
+app.config.from_object('config')
 
 db.init_app(app)
 bcrypt.init_app(app)
