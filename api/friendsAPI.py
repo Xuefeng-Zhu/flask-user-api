b81346c9845eb319e6058946ac53072a7f214a38
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-01-10 16:56:42 -0600

    add delete friends and add firend request

--- api/friendsAPI.py
+++ api/friendsAPI.py
@@ -1,6 +1,8 @@
 from flask import request, abort
 from flask.ext.restful import Resource, reqparse
 from model.friends import Friends
+from model.profile import Profile
+from model.request import Request
 from util.userAuth import auth_required
 from util.serialize import friends_list_serialize
 
@@ -11,12 +13,11 @@ class FriendsListAPI(Resource):
     @auth_required
     def get(self, user_id):
         # load profile 
-        friends =  Friends.objects(user=user_id).only('friends_list').select_related()
-
-        if len(friends) is 0:
+        friends =  Friends.objects(user=user_id).only('friends_list').first()
+        if friends is None:
             return {}
 
-        return friends_list_serialize(friends[0].friends_list)
+        return friends_list_serialize(friends.friends_list)
 
     @auth_required
     def post(self, user_id):
@@ -26,12 +27,34 @@ class FriendsListAPI(Resource):
         if profile_id is None:
             abort(400)
 
-        success = Friends.objects(user=user_id).only('friends_list').update_one(add_to_set__friends_list=profile_id)
+        friend_profile = Profile.objects(id=profile_id).first()
+        if friend_profile is None:
+            abort(400)
 
+        success = Friends.objects(user=user_id).only('friends_list').update_one(add_to_set__friends_list=profile_id)
         if success is 0:
             friends = Friends(user=user_id, friends_list=[profile_id])
             friends.save()
 
+        user_profile = Profile.objects(user=user_id).first()
+        success = Request.objects(user=friend_profile.user).update_one(add_to_set__requests_list=user_profile)
+        if success is 0:
+            friend_request = Request(user=friend_profile.user, type='friends', requests_list=[user_profile])
+            friend_request.save()
+
         return {'status': 'success', 'message': 'The user has been added to your friend list'}
 
+    @auth_required
+    def delete(self, user_id):
+        args = friendsParser.parse_args()
+        profile_id = args['profile_id']
+
+        if profile_id is None:
+            abort(400)
+
+        success = Friends.objects(user=user_id).only('friends_list').update_one(pull__friends_list=profile_id)
+        if success is 0:
+            abort(400)
+
+        return {'status': 'success', 'message': 'The user has been delete from your friend list'}
 
---
+++ model/request.py
@@ -0,0 +1,6 @@
+from model import db
+
+class Request(db.Document):
+	user = db.ReferenceField('User')
+	type = db.StringField(required=True)
+	requests_list = db.ListField(db.ReferenceField('Profile'))
