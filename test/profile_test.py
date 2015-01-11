15fe535114dd1b423bcac12101138d2a413bad51
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-01-10 20:38:38 -0600

    add profile test

--- test/profile_test.py
+++ test/profile_test.py
@@ -2,7 +2,30 @@ import requests
 import json
 from login_test import login
 
+profile_url = 'http://localhost:5000/profile'
+profile_data = {
+	'username': 'test',
+	'school': 'UIUC',
+	'intro': 'test intro',
+	'profile_icon': None
+}
+
+def edit_profile(token):
+	headers = {'token': token} 
+	r = requests.post(profile_url, headers=headers, data = profile_data)
+	result = json.loads(r.content)
+	assert result == profile_data
+
+def load_profile(token):
+	headers = {'token': token} 
+	r = requests.get(profile_url, headers=headers)
+	result = json.loads(r.content)
+	assert result == profile_data
+
 if __name__ == '__main__':
 	login = login()
 	token = login['token']
-	print token
+
+	edit_profile(token)
+	load_profile(token)
+
