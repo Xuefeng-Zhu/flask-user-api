5e2d8c6e4a504b1eddf0c01106658d39e20db57e
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-01-10 21:05:55 -0600

    add search profile test

--- test/profile_test.py
+++ test/profile_test.py
@@ -3,11 +3,15 @@ import json
 from login_test import login
 
 profile_url = 'http://localhost:5000/profile'
+search_profile_url = 'http://localhost:5000/search_profile'
 profile_data = {
 	'username': 'test',
 	'school': 'UIUC',
 	'intro': 'test intro',
 	'profile_icon': None
+}
+search_data = {
+	'school': 'UIUC'
 }
 
 def edit_profile(token):
@@ -22,10 +26,17 @@ def load_profile(token):
 	result = json.loads(r.content)
 	assert result == profile_data
 
+def search_profile(token):
+	headers = {'token': token} 
+	r = requests.get(search_profile_url, headers=headers, data = search_data)
+	result = json.loads(r.content)
+	assert result[0]['school'] == search_data['school']
+
 if __name__ == '__main__':
 	login = login()
 	token = login['token']
 
 	edit_profile(token)
 	load_profile(token)
+	search_profile(token)
 
