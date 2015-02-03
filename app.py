2b79f649d242b06951430cb5f57f953b68ca006b
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-02-03 17:36:03 -0600

    add comment to app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from api.friendsAPI import FriendsListAPI, FriendsRequestAPI
 from api.passwordAPI import ChangePasswordAPI, ForgetPasswordAPI
 from api.postAPI import PostAPI
 
+# load configuration and bootstrap flask
 app = Flask(__name__)
 app.config.from_object('config')
 
@@ -21,12 +22,12 @@ api = Api(app)
 api.decorators = [cors.crossdomain(origin='*',
                                    headers='my-header, accept, content-type, token')]
 
+# add endpoints to flask restful api 
 api.add_resource(UserAPI, '/create_user')
 api.add_resource(LoginAPI, '/login')
 api.add_resource(FBUserAPI, '/fb_create_user')
 api.add_resource(FBLoginAPI, '/fb_login')
 api.add_resource(ActivateAPI, '/activate_account')
-
 
 api.add_resource(ChangePasswordAPI, '/change_password')
 api.add_resource(ForgetPasswordAPI, '/forget_password')
